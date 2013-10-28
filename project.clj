cee53c2fab5d680d9877d036553de917db88c439
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-10-28 14:09:07 +0100

    reorganized by mooving develpment stuff under the profiles.clj file

--- project.clj
+++ project.clj
@@ -1,39 +1,20 @@
 (defproject org.clojars.magomimmo/domina "1.0.3-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
-  :min-lein-version "2.1.2"
-  :source-paths ["src/cljs"]
+  :min-lein-version "2.2.0"
+
+  :source-paths ["src/clj"]
+
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]]
+                 [org.clojure/clojurescript "0.0-1934"]]
+
   :plugins [[lein-cljsbuild "0.3.4"]]
+
   :hooks [leiningen.cljsbuild]
-  :cljsbuild {:builds [{:jar true
-                        :compiler {:libs ["goog/dom/query.js"]
-                                   :pretty-print true
-                                   :output-dir ".cljsbuild/domina"
-                                   :output-to "public/domina.js"}}
-                       {:source-paths ["test/cljs"]
-                        :compiler  {:libs ["goog/dom/query.js"]
-                                    :pretty-print true
-                                    :optimizations :none
-                                    :output-dir "public/build_no_opt"
-                                    :output-to "public/test_no_opt.js"}}
-                       {:source-paths ["test/cljs"]
-                        :compiler  {:libs ["goog/dom/query.js"]
-                                    :optimizations :whitespace
-                                    :pretty-print true
-                                    :output-dir ".cljsbuild/whitespace"
-                                    :output-to "public/test_whitespace.js"}}
-                       {:source-paths ["test/cljs"]
-                        :compiler  {:libs ["goog/dom/query.js"]
-                                    :optimizations :simple
-                                    :pretty-print true
-                                    :output-dir ".cljsbuild/simple"
-                                    :output-to "public/test_simple.js"}}
-                       {:source-paths ["test/cljs"]
-                        :compiler  {:libs ["goog/dom/query.js"]
-                                    :optimizations :advanced
-                                    :pretty-print true
-                                    :output-dir ".cljsbuild/advanced"
-                                    :output-to "public/test_advanced.js"}}]})
 
-
+  :cljsbuild {:builds {:deploy
+                       {:source-paths ["src/cljs"]
+                        :jar true
+                        :compiler
+                        {:output-to "dev-resources/public/js/deploy.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}}})
