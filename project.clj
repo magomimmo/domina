594b19b1e81e0e60b2721e1e4ccb0d4819261ecc
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-25 12:15:53 +0100

    update deps, plugins and min-lein-version

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.clojars.magomimmo/domina "1.0.3-SNAPSHOT"
+(defproject org.clojars.magomimmo/domina "2.0.0-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
-  :min-lein-version "2.2.0"
+  :min-lein-version "2.5.3"
 
   :source-paths ["src/clj"]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]]
 
-  :plugins [[lein-cljsbuild "0.3.4"]]
+  :plugins [[lein-cljsbuild "1.1.1"]]
 
   :hooks [leiningen.cljsbuild]
 
